cb5d275761e234de71de02e5bff01cacfdf1a641
asmateus <mateus@uninorte.edu.co>
2017-10-01 17:54:36 -0500

    Added profiler to the installed apps

--- devilebay/devilebay/settings.py
+++ devilebay/devilebay/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'profiler.apps.ProfilerConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
